80fcf8661a2f785d62f0325c754e610d83eee1da
Rutger Vos <rutgeraldo@gmail.com>
2012-03-28 02:11:27 +0200

    simplified command line arguments

--- runall.sh
+++ runall.sh
@@ -3,9 +3,7 @@ DATA=data
 RESULTS=results
 SCRIPT=script
 PERL=perl
-TAXA="-t Polymeres:silver -t SestochilusStenochilus:red"
 INFILE=$DATA/Bulbophyllinae.tre
 OUTFILE=$RESULTS/Bulbophyllinae.svg
-SECTIONS=$DATA/secties.txt
 
-$PERL $SCRIPT/printtrees.pl -i $INFILE -f nexus $TAXA -s $SECTIONS -o $OUTFILE
+$PERL $SCRIPT/printtrees.pl -i $INFILE -f nexus -t Polymeres:silver > $OUTFILE
