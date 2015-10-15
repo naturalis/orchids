eb15eaeb42eadaaa01e178b9ad8e404669251593
rvosa <rutgeraldo@gmail.com>
2015-10-15 14:20:46 +0200

    added command line arg handling

--- script/make_nexus.pl
+++ script/make_nexus.pl
@@ -1,25 +1,44 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use Getopt::Long;
 use Bio::Phylo::Factory;
 use Bio::Phylo::IO qw'parse_tree parse_matrix';
 
-my ( $matrixfile, $treefile ) = @ARGV;
+# process command line arguments
+my ( $matrixfile, $treefile, $dataformat, $phyloformat );
+if ( @ARGV == 2 ) {
+	( $matrixfile, $treefile ) = @ARGV;
+	$dataformat = $phyloformat = 'nexus';
+}
+else {
+	GetOptions(
+		'treefile=s'    => \$treefile,
+		'matrixfile=s'  => \$matrixfile,
+		'dataformat=s'  => \$dataformat,
+		'phyloformat=s' => \$phyloformat,
+	);
+}
 
+# read alignment
 my $matrix = parse_matrix(
+	'-type'       => 'dna',
 	'-file'       => $matrixfile,
-	'-format'     => 'nexus',
+	'-format'     => $dataformat,
 	'-as_project' => 1,
 );
 
+# read tree
 my $tree = parse_tree(
 	'-file'       => $treefile,
-	'-format'     => 'nexus',
+	'-format'     => $phyloformat,
 	'-as_project' => 1,
 );
 
+# make taxa block from alignment
 my $taxa = $matrix->make_taxa;
 
+# link tips to taxa
 $tree->visit(sub{
 	my $node = shift;
 	if ( $node->is_terminal ) {
@@ -27,6 +46,7 @@ $tree->visit(sub{
 	}
 });
 
+# merge data into project
 my $fac = Bio::Phylo::Factory->new;
 my $proj = $fac->create_project;
 my $forest = $fac->create_forest( '-taxa' => $taxa );
@@ -34,4 +54,6 @@ $forest->insert($tree);
 $proj->insert($taxa);
 $proj->insert($matrix);
 $proj->insert($forest);
+
+# write to STDOUT
 print $proj->to_nexus;
