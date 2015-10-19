b3e4f74f94cc7570b7189510f17dc3ac3be72712
rvosa <rutgeraldo@gmail.com>
2015-10-19 16:53:30 +0200

    can now give start and end colors and define gene class to root on

--- script/draw_tree.pl
+++ script/draw_tree.pl
@@ -12,13 +12,17 @@ use Bio::Phylo::Util::CONSTANT ':objecttypes';
 
 # process command line arguments
 my ( $width, $height, $predicate, $verbosity ) = ( 1200, 1200, 'hyphy:omega3', WARN );
+my $colors = 'red,purple';
+my $famroot;
 my $infile;
 GetOptions(
 	'infile=s'    => \$infile,
 	'width=i'     => \$width,
 	'height=i'    => \$height,
 	'predicate=s' => \$predicate,
+	'colors=s'    => \$colors,
 	'verbose+'    => \$verbosity,
+	'famroot=s'   => \$famroot,
 );
 
 # instantiate helper objects
@@ -36,16 +40,13 @@ my $tree = parse_tree(
 	'-factory' => $fac,
 );
 my $draw = Bio::Phylo::Treedrawer->new(
-    '-width'   => $width,
-    '-height'  => $height,
-    '-tree'    => $tree,
-    '-format'  => 'svg',
-    '-shape'   => 'rect',
-    '-mode'    => 'phylo',
+	'-width'   => $width,
+	'-height'  => $height,
+	'-tree'	   => $tree,
+	'-format'  => 'svg',
+	'-shape'   => 'rect',
+	'-mode'	   => 'phylo',
 );
-
-# apply midpoint rooting
-$tree->get_midpoint->set_root_below(1);
 
 # get log transformed max value
 my @values;
@@ -74,9 +75,22 @@ $tree->visit(sub{
 	}
 });
 
+# apply midpoint rooting
+if ( $famroot ) {
+	my @tips;
+	$tree->visit(sub{
+		my $n = shift;
+		push @tips, $n if $n->is_terminal and $n->get_generic('fam') eq $famroot;
+	});
+	$tree->get_mrca(\@tips)->set_root_below(1000);
+}
+else {
+	$tree->get_midpoint->set_root_below(1000);
+}
+
 # apply family colors
 my @fams = keys %fam;
-my @colors = generate( scalar(@fams), 'red', 'purple' );
+my @colors = generate( scalar(@fams), split /,/, $colors );
 my %c = map { $fams[$_] => $colors[$_] } 0 .. $#fams;
 $tree->visit(sub{
 	my $node = shift;
