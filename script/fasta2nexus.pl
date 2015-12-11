45749e91348ea4452aaa4948d4636d2adb33217b
rvosa <rutgeraldo@gmail.com>
2015-12-11 16:54:17 +0100

    adding fasta2nexus converter (uses accession numbers as row labels)

---
+++ script/fasta2nexus.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Bio::Phylo::IO qw'parse unparse';
+use Bio::Phylo::Util::CONSTANT ':objecttypes';
+
+my $file = shift;
+my $proj = parse(
+	'-format'     => 'fasta',
+	'-file'       => $file,
+	'-type'       => 'dna',
+	'-as_project' => 1,
+);
+my ($matrix) = @{ $proj->get_items(_MATRIX_) };
+$matrix->visit(sub{
+	my $row = shift;
+	my @parts = split /_/, $row->get_name;
+	$row->set_name($parts[-1]);
+});
+print unparse(
+	'-format' => 'nexus',
+	'-phylo'  => $proj,
+);
+print <<'MRBAYES';
+BEGIN MRBAYES;
+	lset nst=6 rates=invgamma;
+	mcmc ngen=10000 samplefreq=10;
+	sumt burnin=250;
+END;
+MRBAYES
