b3ca9c6cafc1892e5c22b1a9b3ba3d7364e433a6
rvosa <rutgeraldo@gmail.com>
2015-12-15 09:26:35 +0100

    adding script to make GSDI input files

---
+++ script/make_phyloxml.pl
@@ -0,0 +1,127 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Getopt::Long;
+use Bio::DB::GenBank;
+use Bio::Phylo::Factory;
+use Bio::Phylo::Util::Logger ':levels';
+use Bio::Phylo::IO qw'parse_tree parse unparse';
+use Bio::SUPERSMART::Service::MarkersAndTaxaSelector;
+use Bio::Phylo::Util::CONSTANT qw':namespaces :objecttypes';
+
+# process command line arguments
+my $verbosity = WARN;
+my ( $speciestree, $genetree, $outfile );
+GetOptions(
+	'speciestree=s' => \$speciestree,
+	'genetree=s'    => \$genetree,
+	'outfile=s'     => \$outfile,
+	'verbose+'      => \$verbosity,
+);
+
+# instantiate helper objects
+my $db  = Bio::DB::GenBank->new;
+my $fac = Bio::Phylo::Factory->new;
+my $mts = Bio::SUPERSMART::Service::MarkersAndTaxaSelector->new;
+my $log = Bio::Phylo::Util::Logger->new(
+	'-level'  => $verbosity,
+	'-class'  => 'main',
+);
+
+# this is optional...
+if ( $speciestree ) {
+
+	# ... but requires an outfile
+	if ( not $outfile ) {
+		$log->error("need -outfile argument when processing -speciestree");
+		exit(1);
+	}
+
+	# annotate the species tree. we do this using TNRS.
+	$log->info("going to annotate species tree $speciestree");
+	my $sp = parse(
+		'-format' => 'newick',
+		'-file'   => $speciestree,	
+		'-as_project' => 1,
+	);
+	my ($st) = @{ $sp->get_items(_TREE_) };
+	$st->set_namespaces( 'px' => _NS_PHYLOXML_ );
+	my @staxa;
+	for my $tip ( @{ $st->get_terminals } ) {
+
+		# pre-process the name
+		my $name = $tip->get_name;
+		$name =~ s/_/ /g;
+		$name =~ s/-/ /g;
+		$name =~ s/'//g;
+	
+		# do a lookup
+		if ( my $id = $mts->_do_tnrs_search($name) ) {
+			$log->info("$name => $id");
+			$tip->set_name($name);
+			push @staxa, make_taxon($name,$tip,$id);
+		}
+		else {
+			$log->error("Couldn't find name $name");
+		}	
+	}
+	
+	# write output
+	open my $fh, '>', $outfile or die $!;
+	print $fh unparse(
+		'-format' => 'phyloxml',
+		'-phylo'  => $sp,
+	);
+}
+
+# annotate the gene tree. we do this by doing a lookup of the accession
+$log->info("going to annotate gene tree $genetree");
+my $gp = parse(
+	'-format'     => 'nexus',
+	'-file'       => $genetree,
+	'-as_project' => 1,
+);
+my ($gt) = @{ $gp->get_items(_TREE_) };
+$gt->set_namespaces( 'px' => _NS_PHYLOXML_ );
+my @gtaxa;
+for my $tip ( @{ $gt->get_terminals } ) {
+
+	# pre-process the name
+	my $name = $tip->get_name;
+	my @parts = split /_/, $name;
+	$name = $parts[-1];
+	
+	# do a lookup
+	if ( my $seq = $db->get_Seq_by_acc($name) ) {
+		my $id = $seq->species->ncbi_taxid;
+		$name  = $seq->species->binomial . " $name";
+		$name  =~ s/ /_/g;
+		$tip->set_name($name);
+		$log->info("$name => $id");
+		push @gtaxa, make_taxon($name,$tip,$id);
+	}
+	else {
+		$log->error("Couldn't find sequence $name");
+	}
+}
+
+# write output
+print unparse(
+	'-format' => 'phyloxml',
+	'-phylo'  => $gp,
+);
+
+sub make_taxon {
+	my ( $name, $node, $id ) = @_;
+	my $taxon = $fac->create_taxon(
+		'-name'  => $name,
+		'-nodes' => [ $node ],
+	);
+	$taxon->set_meta_object(
+		'px:id' => $fac->create_meta(
+			'-namespaces' => { 'px' => _NS_PHYLOXML_ },
+			'-triple'     => { 'px:uniprot' => $id   },
+		)
+	);	
+	return $taxon;
+}
