20a4aee826788e27674309f41d912d8de14746f2
rvosa <rutgeraldo@gmail.com>
2015-12-16 17:25:45 +0100

    adjusted input/output paths

--- phyml.sh
+++ phyml.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-# Usage: ./phyml.sh input.phy
+# Usage: ./phyml.sh
 
 FAMILIES="A_E_AE AP3_PI C_D"
-DATA=data/selection/2015-10-15/
+INPUT=data/selection/2015-12-15
+OUTPUT=data/genetrees/2015-12-11
 
+# iterate over gene families (local folder names)
 for FAM in $FAMILIES; do
 
 	# convert codon alignment to phylip for phyml
-	perl script/fasta2phylip.pl $DATA/$FAM/codon.aln.fasta > $DATA/$FAM/codon.aln.phy
+	perl script/fasta2phylip.pl $INPUT/$FAM/codon.aln.fasta > $OUTPUT/$FAM/codon.aln.phy
 
 	# nt        = nucleotide
 	# -f e      = estimate base frequencies
@@ -15,6 +17,6 @@ for FAM in $FAMILIES; do
 	# --pinv e  = estimate proportion of invariant sites
 	# --alpha e = estimate gamma distribution shape parameter
 	# --search BEST = best topology search result from among NNI and SPR
-	phyml --input $DATA/$FAM/codon.aln.phy --datatype nt --sequential --model GTR -f e --ts/tv e --pinv e --alpha e --search BEST --quiet
+	phyml --input $OUTPUT/$FAM/codon.aln.phy --datatype nt --sequential --model GTR -f e --ts/tv e --pinv e --alpha e --search BEST --quiet
 
 done
