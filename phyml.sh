b236d91c5376f0e8134a576da1672679a9b64d8d
rvosa <rutgeraldo@gmail.com>
2015-12-16 12:18:40 +0100

    now prepares phylip input files

--- phyml.sh
+++ phyml.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 # Usage: ./phyml.sh input.phy
-# nt        = nucleotide
-# -f e      = estimate base frequencies
-# --ts/tv e = estimate transition/transversion ratio
-# --pinv e  = estimate proportion of invariant sites
-# --alpha e = estimate gamma distribution shape parameter
-# --search BEST = best topology search result from among NNI and SPR
-phyml --input $1 --datatype nt --sequential --model GTR -f e --ts/tv e --pinv e --alpha e --search BEST --quiet
+
+FAMILIES="A_E_AE AP3_PI C_D"
+DATA=data/selection/2015-10-15/
+
+for FAM in $FAMILIES; do
+
+	# convert codon alignment to phylip for phyml
+	perl script/fasta2phylip.pl $DATA/$FAM/codon.aln.fasta > $DATA/$FAM/codon.aln.phy
+
+	# nt        = nucleotide
+	# -f e      = estimate base frequencies
+	# --ts/tv e = estimate transition/transversion ratio
+	# --pinv e  = estimate proportion of invariant sites
+	# --alpha e = estimate gamma distribution shape parameter
+	# --search BEST = best topology search result from among NNI and SPR
+	phyml --input $DATA/$FAM/codon.aln.phy --datatype nt --sequential --model GTR -f e --ts/tv e --pinv e --alpha e --search BEST --quiet
+
+done
