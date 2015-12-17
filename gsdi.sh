8e27afde45ee6c662b714cb67f0bf6dc03387c40
rvosa <rutgeraldo@gmail.com>
2015-12-17 14:44:59 +0100

    now removes GSDI files before re-running

--- gsdi.sh
+++ gsdi.sh
@@ -2,6 +2,7 @@
 JAR=bin/forester_1038.jar
 SPECIESTREE=data/speciestree/cladogram
 GENETREES=data/genetrees/2015-12-11/
+ALIGNMENTS=data/selection/2015-12-15/
 FAMILIES="AP3_PI A_E_AE C_D"
 
 # https://sites.google.com/site/cmzmasek/home/software/forester/gsdi
@@ -18,9 +19,10 @@ for FAM in $FAMILIES; do
 
 	# generate input files
 	G=$GENETREES/$FAM/codon.aln.phy_phyml_tree
-	$PHYLOXML -g $G.txt -v > $G.xml
+	$PHYLOXML -g $G.txt -a $ALIGNMENTS/$FAM/codon.aln.fasta -v > $G.xml
 
 	# run GSDI
+	rm ${G}.gsdi_gsdi_log.txt ${G}.gsdi_species_tree_used.xml
 	$GSDI $G.xml $SPECIESTREE.xml $G.gsdi.xml
 
 done
