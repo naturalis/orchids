dbb81cbffcb6eb12ae255e7cf20909af437d799e
Rutger Vos <rutgeraldo@gmail.com>
2016-01-07 14:52:33 +0100

    Delete drawtree.sh

--- drawtree.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DATA=data/viz
-RESULTS=results/viz
-SCRIPT=script
-PERL=perl
-INFILE=$DATA/Bulbophyllinae.tre
-OUTFILE=$RESULTS/Bulbophyllinae.svg
-
-$PERL $SCRIPT/printtrees.pl -i $INFILE -f nexus -t Polymeres:silver > $OUTFILE
